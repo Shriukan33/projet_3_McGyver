db85865da099ba20f0cc1b757fcf445b31d703f8
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-13 17:58:22 +0200

    Added item class

--- McGyver_escape/items.py
+++ McGyver_escape/items.py
@@ -1,1 +1,17 @@
 import pygame
+from pathlib import Path
+
+# Set this constant to make it simple to point to ressources folder
+ASSETS = str(Path(__file__).resolve().parent) + "/ressources/"
+
+
+class Items(pygame.sprite.Sprite):
+
+    def __init__(self, game, asset_name, x_pos, y_pos):
+        super().__init__()
+        self.game = game
+        self.image = pygame.image.load(ASSETS + asset_name)
+        self.image = pygame.transform.scale(self.image, (32, 32))
+        self.rect = self.image.get_rect()
+        self.rect.x = x_pos
+        self.rect.y = y_pos

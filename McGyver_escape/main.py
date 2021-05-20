f4f6f6af84fc05333c9c1b129121917249ca3214
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-20 12:05:41 +0200

    Extracted game logic out of main loop for better visibility

--- McGyver_escape/main.py
+++ McGyver_escape/main.py
@@ -3,6 +3,110 @@ import pygame
 from game import Game
 
 
+def render_win_screen(clock, SCREEN, FPS):
+    """Render win screen and exit message"""
+    clock.tick(FPS)
+    SCREEN.fill((0, 0, 0))
+
+    MESSAGE_COLOR = (255, 255, 255)
+
+    win_text = pygame.font.SysFont(None, 36, True)
+    win_text_img = win_text.render("YOU WON !", True, MESSAGE_COLOR)
+    text_rect = win_text_img.get_rect(center=(240, 200))
+    SCREEN.blit(win_text_img, text_rect)
+
+    win_subtext = pygame.font.SysFont(None, 24)
+    win_subtext_img = win_subtext.render(
+        "Press any key to exit the game.", True, MESSAGE_COLOR)
+    text_rect = win_subtext_img.get_rect(center=(240, 240))
+    SCREEN.blit(win_subtext_img, text_rect)
+
+    # Updates the screen
+    pygame.display.flip()
+
+
+def render_lose_screen(clock, SCREEN, FPS):
+    """render lose screen, advice and exit message"""
+    clock.tick(FPS)
+    SCREEN.fill((0, 0, 0))
+
+    MESSAGE_COLOR = (255, 255, 255)
+
+    lose_text = pygame.font.SysFont(None, 36, True)
+    lose_text_img = lose_text.render("You lost !", True, MESSAGE_COLOR)
+    text_rect = lose_text_img.get_rect(center=(240, 200))
+    SCREEN.blit(lose_text_img, text_rect)
+
+    instruction_text = pygame.font.SysFont(None, 30)
+    instruction_text_img = instruction_text.render(
+        "Get all 3 items before fighting the guard !",
+        True, MESSAGE_COLOR)
+    text_rect = instruction_text_img.get_rect(center=(240, 240))
+    SCREEN.blit(instruction_text_img, text_rect)
+
+    lose_subtext = pygame.font.SysFont(None, 24)
+    lose_subtext_img = lose_subtext.render(
+        "Press any key to exit the game.", True, MESSAGE_COLOR)
+    text_rect = lose_subtext_img.get_rect(center=(240, 280))
+    SCREEN.blit(lose_subtext_img, text_rect)
+    pygame.display.flip()
+
+
+def player_movements(event, game):
+    """Handles Player directional movements"""
+    if event.key == pygame.K_UP:
+        game.player.moveUp()
+    if event.key == pygame.K_DOWN:
+        game.player.moveDown()
+    if event.key == pygame.K_RIGHT:
+        game.player.moveRight()
+    if event.key == pygame.K_LEFT:
+        game.player.moveLeft()
+
+
+def screen_update(clock, FPS, SCREEN, game):
+    clock.tick(FPS)
+    # Filling the bacground in black, it's dungeon-like
+    SCREEN.fill((0, 0, 0))
+    # Draws all assets
+    game.all_walls.draw(SCREEN)
+    game.all_items.draw(SCREEN)
+    game.all_guards.draw(SCREEN)
+    game.all_exits.draw(SCREEN)
+    game.all_players.draw(SCREEN)
+
+    # Inventory display, clears the objective
+    if game.inventory != 3:
+        inventory_font = pygame.font.SysFont(None, 26)
+        inventory_font_img = inventory_font.render(
+            "Gather all 3 items to craft a seringe ! ({}/3)".format(
+                game.inventory),
+            True, (255, 255, 255))
+        SCREEN.blit(inventory_font_img, (10, 485))
+    elif game.inventory == 3:
+        inventory_font = pygame.font.SysFont(None, 26)
+        inventory_font_img = inventory_font.render(
+            "Seringe crafted ! Put the guard to sleep and escape!",
+            True, (255, 255, 255))
+        SCREEN.blit(inventory_font_img, (10, 485))
+
+    pygame.display.flip()
+
+
+def interaction_check(game):
+    # Check if Mc giver is picking up an object
+    for item in game.all_items:
+        item.check_pick_up()
+
+    # Check If McGyver is engaging a fight with the guard
+    for guard in game.all_guards:
+        guard.check_fight()
+
+    # Check if Mc Gyver is out !
+    for exit in game.all_exits:
+        exit.check_exit()
+
+
 def main():
 
     # Initialize the pygame modules
@@ -13,7 +117,7 @@ def main():
     SCREEN = pygame.display.set_mode((32*15, 32*16))
     # Framerate control
     clock = pygame.time.Clock()
-    FPS = 24
+    FPS = 60
 
     running = True
 
@@ -25,42 +129,9 @@ def main():
 
         while game.main_game:
             clock.tick(FPS)
-            # Filling the bacground in black, it's dungeon-like
-            SCREEN.fill((0, 0, 0))
-            # Draws all assets
-            game.all_walls.draw(SCREEN)
-            game.all_items.draw(SCREEN)
-            game.all_guards.draw(SCREEN)
-            game.all_exits.draw(SCREEN)
-            game.all_players.draw(SCREEN)
 
-            # Inventory display, clears the objective
-            if game.inventory != 3:
-                inventory_font = pygame.font.SysFont(None, 26)
-                inventory_font_img = inventory_font.render(
-                    "Gather all 3 items to craft a seringe ! ({}/3)".format(
-                        game.inventory),
-                    True, (255, 255, 255))
-                SCREEN.blit(inventory_font_img, (10, 485))
-            elif game.inventory == 3:
-                inventory_font_img = inventory_font.render(
-                    "Seringe crafted ! Put the guard to sleep and escape!",
-                    True, (255, 255, 255))
-                SCREEN.blit(inventory_font_img, (10, 485))
-
-            # Check if Mc giver is picking up an object
-            for item in game.all_items:
-                item.check_pick_up()
-
-            # Check If McGyver is engaging a fight with the guard
-            for guard in game.all_guards:
-                guard.check_fight()
-
-            # Check if Mc Gyver is out !
-            for exit in game.all_exits:
-                exit.check_exit()
-
-            pygame.display.flip()
+            interaction_check(game)
+            screen_update(clock, FPS, SCREEN, game)
 
             # This loops through all events of the game
             for event in pygame.event.get():
@@ -74,42 +145,13 @@ def main():
                 # Placed here, holding the key will not produce
                 # several movements.
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        game.player.moveUp()
-                    if event.key == pygame.K_DOWN:
-                        game.player.moveDown()
-                    if event.key == pygame.K_RIGHT:
-                        game.player.moveRight()
-                    if event.key == pygame.K_LEFT:
-                        game.player.moveLeft()
+                    player_movements(event, game)
 
         # Lose screen enabled when you encounter a guard and don't have all
         # items
         while game.lose_screen:
 
-            clock.tick(FPS)
-            SCREEN.fill((0, 0, 0))
-
-            MESSAGE_COLOR = (255, 255, 255)
-
-            lose_text = pygame.font.SysFont(None, 36, True)
-            lose_text_img = lose_text.render("You lost !", True, MESSAGE_COLOR)
-            text_rect = lose_text_img.get_rect(center=(240, 200))
-            SCREEN.blit(lose_text_img, text_rect)
-
-            instruction_text = pygame.font.SysFont(None, 30)
-            instruction_text_img = instruction_text.render(
-                "Get all 3 items before fighting the guard !",
-                True, MESSAGE_COLOR)
-            text_rect = instruction_text_img.get_rect(center=(240, 240))
-            SCREEN.blit(instruction_text_img, text_rect)
-
-            lose_subtext = pygame.font.SysFont(None, 24)
-            lose_subtext_img = lose_subtext.render(
-                "Press any key to exit the game.", True, MESSAGE_COLOR)
-            text_rect = lose_subtext_img.get_rect(center=(240, 280))
-            SCREEN.blit(lose_subtext_img, text_rect)
-            pygame.display.flip()
+            render_lose_screen(clock, SCREEN, FPS)
 
             for event in pygame.event.get():
 
@@ -125,24 +167,7 @@ def main():
         # Win screen enabled when getting to the exit
         while game.win_screen:
 
-            clock.tick(FPS)
-            SCREEN.fill((0, 0, 0))
-
-            MESSAGE_COLOR = (255, 255, 255)
-
-            win_text = pygame.font.SysFont(None, 36, True)
-            win_text_img = win_text.render("YOU WON !", True, MESSAGE_COLOR)
-            text_rect = win_text_img.get_rect(center=(240, 200))
-            SCREEN.blit(win_text_img, text_rect)
-
-            win_subtext = pygame.font.SysFont(None, 24)
-            win_subtext_img = win_subtext.render(
-                "Press any key to exit the game.", True, MESSAGE_COLOR)
-            text_rect = win_subtext_img.get_rect(center=(240, 240))
-            SCREEN.blit(win_subtext_img, text_rect)
-
-            # Updates the screen
-            pygame.display.flip()
+            render_win_screen(clock, SCREEN, FPS)
 
             for event in pygame.event.get():
 
